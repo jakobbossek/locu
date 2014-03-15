e74aca01ffff0fa87fec5c8f25e680dc905ee789
Jakob Bossek <info@jakobbossek.de>
2014-03-15 09:55:32 +0100

    add Herfindahl-hirschmann measure

---
+++ R/herfindahl.R
@@ -0,0 +1,22 @@
+#' Computes the Herfindahl-(Hirschmann) index measure of concentration.
+#'
+#' @param x [\code{numeric}]\cr
+#'   Vector of (positive) numbers.
+#' @return [\code{numeric(1)}]
+#'   Herfindahl index.
+#' @export
+#' @examples
+#' x = c(1, 5, 19, 7, 8)
+#' H = herfindahl(x)
+#' print(H)
+herfindahl = function(x) {
+  #FIXME: maybe later deal with NAs
+  checkArg(x, cl = "numeric", lower = 0, min.len = 1, na.ok = FALSE)
+  if(any(is.infinite(x))) {
+    stopf("All values must be finite!")
+  }
+  n = length(x)
+  proportions = x / sum(x)
+  H = sum(proportions^2)
+  return(H)
+}
---
+++ inst/tests/test_herfindahl.R
@@ -0,0 +1,14 @@
+context("Herfindahl index")
+
+test_that("Herfindahl index returns expected values", {
+  x = c(rep(0, 10), 11)
+  expect_equal(herfindahl(x), 1, "Herfindahl index is maximal if monopol is given.")
+
+  x = rep(1, 10)
+  expect_equal(herfindahl(x), 1 / length(x), "Herfindahl index is minimal, if proportions are equally distributed.")
+
+  for (i in seq(10)) {
+    x = abs(rnorm(10, mean = 10, sd = 5))
+    expect_true(herfindahl(x) > 0)
+  }
+})
