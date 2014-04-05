5d008d73bf753a120169fb3fa69bc3d1f925f9a0
Jakob Bossek <info@jakobbossek.de>
2014-04-05 09:57:25 +0200

    add .Rbuildignore

--- test_specific.R
+++ test_specific.R
@@ -6,6 +6,8 @@ load_all(".", reset = TRUE)
 
 x = c(2,3,4,5,6,8,8,30)
 res = locu(x)
+pl = autoplot(res, highlight.below.curve = TRUE, highlight.above.curve = TRUE, point.size = 10)
+
 
 x = c(rep(0, 10), 100)
 print(gini(x))
